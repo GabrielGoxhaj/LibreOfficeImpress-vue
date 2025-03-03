4ddc32e0fa73b678dc6d72bbc6a72eb86feeec33
Gabriel Goxhaj <gabrielgoxhaj2013@hotmail.it>
2025-03-03 16:34:59 +0100

    features: icone per Dropdown Inserisci

--- src/components/MenuDropdowns/InserisciDropdown.vue
+++ src/components/MenuDropdowns/InserisciDropdown.vue
@@ -1,28 +1,28 @@
 <template>
     <div id="dropdownInserisci" class="dropdownInserisci" :style="style">
-        <a>Immagine...</a>
-        <a>Audio o video...</a>
-        <a>Grafico...</a>
-        <a>Tabella...</a>
-        <a>Media</a>
-        <a>Oggetto OLE</a>
-        <a>Forma</a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-immagine.png">Immagine...</div></a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-audio-video.png">Audio o video...</div></a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-grafico.png">Grafico...</div></a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-tabella.png">Tabella...</div></a>
+        <a style="display: flex; justify-content: space-between;"><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-media.png">Media</div><g>&#x1F782;</g></a>
+        <a style="display: flex; justify-content: space-between;"><div><img src="../../assets/Impress/icons/icons_dropdown/vuoto-16x16.png">Oggetto OLE</div><g>&#x1F782;</g></a>
+        <a style="display: flex; justify-content: space-between;"><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-forma.png">Forma</div><g>&#x1F782;</g></a>
         <hr />
-        <a>Linea guida di cattura...</a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/vuoto-16x16.png">Linea guida di cattura...</div></a>
         <hr />
-        <a>Casella di testo</a>
-        <a>Commento</a>
-        <a>Fontwork...</a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-casella-testo.png">Casella di testo</div></a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-commento.png">Commento</div></a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-fontwork.png">Fontwork...</div></a>
         <hr />
-        <a>Collegamento...</a>
-        <a>Carattere speciale...</a>
-        <a>Segno di formattazione</a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-collegamento.png">Collegamento...</div></a>
+        <a class="disabled"><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-carattere-speciale.png">Carattere speciale...</div></a>
+        <a class="disabled" style="display: flex; justify-content: space-between;"><div><img src="../../assets/Impress/icons/icons_dropdown/vuoto-16x16.png">Segno di formattazione</div><g>&#x1F782;</g></a>
         <hr />
-        <a>Numero diapositiva</a>
-        <a>Comando di campo</a>
-        <a>Intestazione e piè di pagina...</a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-numero.png">Numero diapositiva</div></a>
+        <a style="display: flex; justify-content: space-between;"><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-comando.png">Comando di campo</div><g>&#x1F782;</g></a>
+        <a><div><img src="../../assets/Impress/icons/icons_dropdown/vuoto-16x16.png">Intestazione e piè di pagina...</div></a>
         <hr />
-        <a>Controlli per formulari</a>
+        <a style="display: flex; justify-content: space-between;"><div><img src="../../assets/Impress/icons/icons_dropdown/inserisci/inserisci-controlli.png">Controlli per formulario</div><g>&#x1F782;</g></a>
     </div>
 </template>
 
